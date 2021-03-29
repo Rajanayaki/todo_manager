02453b1a6a26808cac3bc9249d6e986652e410ed
Rajanayaki <rajanayaki4444@gmail.com>
2021-03-29 13:40:13 -0700

    CRUD features for Todo items

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class TodosController < ApplicationController
   def index
     render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string} .join("\n")
@@ -8,4 +10,26 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     render plain: todo.to_pleasant_string
   end
+
+  def create 
+    todos_text = params[:todos_text]
+    due_date= Date.today + (params[:due_date]).to_i
+    todo= Todo.new( todo_text: todos_text, due_date: due_date , completed: false)
+    todo.save
+  end
+  
+  def update
+    id=params[:id]
+    todo=Todo.find(id)
+    todo.completed=true
+    todo.save
+  end
+  
+  def destroy
+    id= params[:id]
+    todo = Todo.find(id)
+    todo.destroy
+  end 
+
+  
 end
