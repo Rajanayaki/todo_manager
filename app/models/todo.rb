71a3b72cdc95e7209efd570a8445cfb563a429ae
Rajanayaki <rajanayaki4444@gmail.com>
2021-04-01 17:50:46 +0530

    ERB and assets pipelines are added

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,32 @@
+require 'date'
 class Todo < ActiveRecord::Base
-    def to_pleasant_string
-        is_completed = completed ? "[X]": "[]"
-        "#{id}. #{due_date.to_s(:long)} , #{todo_text} #{is_completed}"
-    end
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+  
+  def self.completed
+    all.where(completed: true)
+  end 
+
+  def self.show_list
+    puts "My Todo-list"
+    puts "Overdue"
+    puts overdue.show_list
+    puts "Due Today"
+    puts due_today.show_list
+    puts "Due Later"
+    puts due_later.show_list
+  end
 end
