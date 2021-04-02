4a42a76d52d3a3965656aef627df38bfce6171a5
Rajanayaki <rajanayaki4444@gmail.com>
2021-04-02 16:25:20 +0530

    Changed the Users migration

--- db/migrate/20210330145605_create_users.rb
+++ db/migrate/20210330145605_create_users.rb
@@ -3,10 +3,10 @@ require 'active_record'
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :first_name
+      t.string: last_name
       t.string :email
-      t.string :password
-
+      t.string :password_digest
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,10 @@ ActiveRecord::Schema.define(version: 2021_03_30_145605) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
